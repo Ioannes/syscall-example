67aecf0075e8642c51f72c6063a477876ee4b033
Ioannes <326778+Ioannes@users.noreply.github.com>
2025-03-12 13:20:39 +0300

    Typo

--- syscal-run.s
+++ syscal-run.s
@@ -7,7 +7,7 @@ message:
 _start:
     # 32 bit syscall SYS_write
     mov $4, %eax        # Put int(4) to eax register for execute "Write syscall"
-    mov $1, %ebx        # Put int(1) to ebx regisater for writing to stdout
+    mov $1, %ebx        # Put int(1) to ebx register for writing to stdout
     mov $message, %ecx  # Put Pointer aggress to ecx register
     mov $14, %edx       # Put data length to edx register
     int $0x80           # Call interrupt!!
