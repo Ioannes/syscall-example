c90cf28b24650daafab60cab2a43417e1771e4c1
Igor Kulakov <kulakov.igor@>
2025-03-12 13:15:31 +0300

    Add more comments

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Используется 32битное API Linux
 
 https://github.com/torvalds/linux/blob/v4.3/arch/x86/entry/syscalls/syscall_32.tbl
+
+Какие именно данные в какой именно регистр надо класть зависит от конкретного системного вызова.
--- syscal-run.s
+++ syscal-run.s
@@ -6,10 +6,10 @@ message:
 
 _start:
     # 32 bit syscall SYS_write
-    mov $4, %eax        # Write syscall
-    mov $1, %ebx        # write to stdout
-    mov $message, %ecx  # Pointer to string
-    mov $14, %edx       # Length of data
+    mov $4, %eax        # Put int(4) to eax register for execute "Write syscall"
+    mov $1, %ebx        # Put int(1) to ebx regisater for writing to stdout
+    mov $message, %ecx  # Put Pointer aggress to ecx register
+    mov $14, %edx       # Put data length to edx register
     int $0x80           # Call interrupt!!
     # in %eax register left written data lenght
 
